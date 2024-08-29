53149dea4c9c3c74ba7881c8f3dd548f2e609451
Farhan Maulana Syidiq <050140299@ecampus.ut.ac.id>
2024-08-29 23:27:54 +0700

    Menambahkan File

---
+++ footer.php
@@ -0,0 +1,9 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Hak Cipta &copy; 2024 Farhan Maulana Syidiq</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
